98d946e33b685fb5de8458811c1648a71eba4eca
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-09-03 00:54:07 +0100

    Create projects (#1)
    * Create projects
    
    * Update packages
    
    * Add GitHub actions
    
    * Add build workflow
    
    * Fix tests
    
    ---------
    
    Co-authored-by: Guilherme Branco Stracini <guilhermes@outsurance.ie>

---
+++ Src/POCExtractPdfContent/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace POCExtractPdfContent
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Tests/POCExtractPdfContent.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace POCExtractPdfContent.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
