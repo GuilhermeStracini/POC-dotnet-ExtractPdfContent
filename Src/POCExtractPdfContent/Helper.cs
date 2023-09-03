41c10e39648281755fdc13252793f8f02d1903d4
Guilherme Branco Stracini <guilhermes@outsurance.ie>
2023-09-03 02:03:11 +0100

    Add PdfPig

---
+++ Src/POCExtractPdfContent/Helper.cs
@@ -0,0 +1,18 @@
+﻿namespace POCExtractPdfContent;
+
+/// <summary>
+/// Class Helper.
+/// </summary>
+public static class Helper
+{
+    /// <summary>
+    /// Downloads the content.
+    /// </summary>
+    /// <param name="url">The URL.</param>
+    /// <returns>System.Byte[].</returns>
+    public static byte[] DownloadContent(string url)
+    {
+        using var httpClient = new HttpClient();
+        return httpClient.GetByteArrayAsync(url).Result;
+    }
+}
