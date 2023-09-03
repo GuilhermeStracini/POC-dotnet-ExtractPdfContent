4d8b1676e8323ff37463fcad5b7835000c78b8c5
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-09-03 02:02:31 +0100

    Add ITextSharp example (#2)
    Co-authored-by: Guilherme Branco Stracini <guilhermes@outsurance.ie>

---
+++ Src/POCExtractPdfContent/ITextSharpExample.cs
@@ -0,0 +1,46 @@
+﻿using iTextSharp.text.pdf;
+using System.Text;
+
+namespace POCExtractPdfContent;
+
+/// <summary>
+/// Class ITextSharpExample.
+/// Implements the <see cref="POCExtractPdfContent.IPdfExtractor" />
+/// </summary>
+/// <seealso cref="POCExtractPdfContent.IPdfExtractor" />
+public class ITextSharpExample : IPdfExtractor
+{
+    /// <summary>
+    /// Extracts the specified path.
+    /// </summary>
+    /// <param name="path">The path.</param>
+    /// <returns>System.String.</returns>
+    public string Extract(string path)
+    {
+        var stringsList = new List<string>();
+        var reader = new PdfReader(new Uri(path));
+
+        var contentBytes = reader.GetPageContent(1);
+        var tokenizer = new PrTokeniser(new RandomAccessFileOrArray(contentBytes));
+
+        while (tokenizer.NextToken())
+        {
+            if (tokenizer.TokenType == PrTokeniser.TK_STRING)
+            {
+                var currentText = tokenizer.StringValue;
+                currentText = Encoding.UTF8.GetString(
+                    ASCIIEncoding.Convert(
+                        Encoding.Default,
+                        Encoding.UTF8,
+                        Encoding.Default.GetBytes(currentText)
+                    )
+                );
+                stringsList.Add(currentText);
+            }
+        }
+
+        reader.Close();
+
+        return string.Join("", stringsList);
+    }
+}
--- Src/POCExtractPdfContent/Program.cs
+++ Src/POCExtractPdfContent/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace POCExtractPdfContent
+﻿namespace POCExtractPdfContent;
+
+/// <summary>
+/// Class Program.
+/// </summary>
+internal static class Program
 {
-    internal static class Program
+    /// <summary>
+    /// The example URL
+    /// </summary>
+    private const string ExampleUrl =
+        "https://www2.nuclea.com.br/Monitoramento/Participantes_Homologados.pdf";
+
+    /// <summary>
+    /// Defines the entry point of the application.
+    /// </summary>
+    /// <param name="args">The arguments.</param>
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine("Hello, World!");
+
+        var extractor = new ITextSharpExample();
+        var content = extractor.Extract(ExampleUrl);
+
+        Console.WriteLine(content);
     }
 }
