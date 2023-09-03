3a54944eb9b3650b1e2dfd7eb2787db3cf1c40ee
Guilherme Branco Stracini <guilhermes@outsurance.ie>
2023-09-03 02:01:33 +0100

    Add interface

---
+++ Src/POCExtractPdfContent/IPdfExtractor.cs
@@ -0,0 +1,14 @@
+﻿namespace POCExtractPdfContent;
+
+/// <summary>
+/// Interface IPdfExtractor
+/// </summary>
+public interface IPdfExtractor
+{
+    /// <summary>
+    /// Extracts the specified path.
+    /// </summary>
+    /// <param name="path">The path.</param>
+    /// <returns>System.String.</returns>
+    string Extract(string path);
+}
