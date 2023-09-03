dba6cd33b733020b340d6cb02e391874521caf00
Guilherme Branco Stracini <guilhermes@outsurance.ie>
2023-09-03 02:01:18 +0100

    Fix program

--- Src/POCExtractPdfContent/Program.cs
+++ Src/POCExtractPdfContent/Program.cs
@@ -1,10 +1,20 @@
 ﻿namespace POCExtractPdfContent;
 
+/// <summary>
+/// Class Program.
+/// </summary>
 internal static class Program
 {
+    /// <summary>
+    /// The example URL
+    /// </summary>
     private const string ExampleUrl =
         "https://www2.nuclea.com.br/Monitoramento/Participantes_Homologados.pdf";
 
+    /// <summary>
+    /// Defines the entry point of the application.
+    /// </summary>
+    /// <param name="args">The arguments.</param>
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
